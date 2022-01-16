9d0a0301281b16ed7273c88898faa547be06b7a6
Rahul-Tyagi12 <61530246+Rahul-Tyagi12@users.noreply.github.com>
2022-01-16 16:25:40 +0530

    Update update.py

--- update.py
+++ update.py
@@ -37,7 +37,7 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except TypeError:
-    UPSTREAM_REPO = "https://github.com/Rahul-Tyagi12/winterBack"
+    UPSTREAM_REPO = "https://github.com/Rahul-Tyagi12/wintertest"
 
 if os.path.exists('.git'):
     subprocess.run(["rm", "-rf", ".git"])
